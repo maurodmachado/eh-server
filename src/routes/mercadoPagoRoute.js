bb70d902259d7b9d8240e5ccacc2f78968b89317
Mauro <machadomauro.cft@gmail.com>
2022-02-20 02:32:12 -0300

    .

--- src/routes/mercadoPagoRoute.js
+++ src/routes/mercadoPagoRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controllers/paymentController');
+const paymentController = require('../controllers/PaymentController');
 
 router.post('/qr', paymentController.getQR);
 
