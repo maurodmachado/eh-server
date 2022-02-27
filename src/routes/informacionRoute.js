a9beb9b4a7e9c8fb1b47e52a7c6d71806d9ecff6
Mauro <machadomauro.cft@gmail.com>
2022-02-27 03:16:04 -0300

    add auth to modificarInfo

--- src/routes/informacionRoute.js
+++ src/routes/informacionRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const informacionController = require('../controllers/informacionController');
+const auth = require('../middleware/auth');
 
 router.get('/', informacionController.getInfo);
 
-router.put('/:id', informacionController.modificarInfo);
+router.put('/:id', auth,informacionController.modificarInfo);
 
 module.exports = router;
